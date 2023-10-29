9679834edd383ecb769e8afcb58b968d4ece9041
ilyxabatko <ilyxabatko@inbox.ru>
2023-10-29 12:39:32 +0400

    server implementation (read from socket one msg)

--- src/serve.rs
+++ src/serve.rs
@@ -1,28 +1,32 @@
-use std::io::{Read, stdout, Write};
+use std::io::{stdout, Read, Write};
 use std::net::TcpListener;
 
 pub fn run(bind_host: &String, port: &u16) -> Result<(), String> {
     let addr = format!("{}:{}", bind_host, port);
-    let listener = TcpListener::bind(addr.clone()).map_err(|_| format!("Failed to bind to {}", addr))?;
+    let listener = TcpListener::bind(&addr)
+        .map_err(|e| format!("Failed to bind to {} address! {}", addr, e))?;
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut s) => {
-                println!("Connection accepted");
+            Ok(mut stream) => {
+                println!("Connection {:?} accepted", &stream);
 
-                let mut buf = [0; 128];
-                let mut read_bytes = 0;
+                // Creates a buffer to read to from the socket (up to 128 bytes)
+                let mut buf = [0u8; 128];
+                let mut read_bytes: usize = 0;
                 while read_bytes == 0 {
-                    read_bytes = s.read(&mut buf).map_err(|_| "failed to read from socket")?;
-                    println!("received bytes {}", read_bytes);
+                    read_bytes = stream
+                        .read(&mut buf)
+                        .map_err(|e| format!("Failed to read from socket: {}", e))?;
+                    println!("Received {} bytes from socket.", read_bytes);
                 }
-                stdout().write(&buf[0..read_bytes]).map_err(|_| "failed to write to stdout")?;
-                stdout().flush().unwrap();
+
+                // Print the buffer to the console cutting off zeroes
+                stdout().write(&buf[0..read_bytes]).map_err(|e| format!("Error printing out the buffer: {}", e))?;
+                stdout().flush().map_err(|e| format!("Error flushing the stdout: {}", e))?;
             }
-            Err(e) => {
-                println!("Error while accepting incoming connection - {}", e);
-            }
-        }
+            Err(e) => println!("Error while accepting connection: {}", e),
+        };
     }
 
     Ok(())
